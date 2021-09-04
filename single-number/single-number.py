735a9cb775b7c02ce5840c895b0f4866ccec487b
Additya Shekhar Dharangaonkar <44835584+adharangaonkar@users.noreply.github.com>
2021-09-04 15:50:52 -0400

    Time: 136 ms (55.48%), Space: 16.8 MB (19.59%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,7 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = {i: nums.count(i) for i in nums}
-        for i, j in d.items():
-#         print(i, j)
+#         d = {i: nums.count(i) for i in nums}
+#         for i, j in d.items():
+# #         print(i, j)
+#             if j == 1:
+#                 return i
+        dict_count = {}
+        for x in nums:
+            if x in dict_count.keys():
+                dict_count[x] +=1
+            else:
+                dict_count[x] =1
+        for i, j in dict_count.items():
             if j == 1:
-                return i
+                return i  
