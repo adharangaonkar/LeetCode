ca25d655694370d96f1fad47f9c0356509a6dac4
Additya Shekhar Dharangaonkar <44835584+adharangaonkar@users.noreply.github.com>
2021-09-04 13:44:12 -0400

    Time: 120 ms (96.14%), Space: 15.6 MB (16.62%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,5 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        b = [a for a in range(len(nums)+1)]
-        num_diff = [a for a in set(b) - set(nums)] 
-        return num_diff[0]
+        # b = [a for a in range(len(nums)+1)]
+        # num_diff = [a for a in set(b) - set(nums)] 
+        # return num_diff[0]
+        
+        a = sum(a for a in range(len(nums) + 1))
+        nums_sum = sum(a for a in nums)
+        return a - nums_sum
+        
