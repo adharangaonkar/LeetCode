afeae603d92cf6c3b7d6102850a2aba64aa06c15
Additya Shekhar Dharangaonkar <44835584+adharangaonkar@users.noreply.github.com>
2021-11-18 18:55:35 -0500

    Time: 1694 ms (40.24%), Space: 0B (100.00%) - LeetHub

--- consecutive-numbers/consecutive-numbers.sql
+++ consecutive-numbers/consecutive-numbers.sql
@@ -1,18 +1,11 @@
 /* Write your T-SQL query statement below */
-WITH CTE1 as (
-    SELECT Id, 
-    Num,
-     LEAD(Num, 1) OVER (ORDER BY Id) AS NextNum,
-     LEAD(Num, 2) OVER (ORDER BY Id) AS SecondNextNum
-      FROM Logs
-)
-,CTE2 as
+
+WITH CTE1 as 
 (
-    SELECT CASE
-    WHEN Num = NextNum and NextNum = SecondNextNum THEN Num
-    ELSE NULL
-    END AS ConsecutiveNums
-    FROM CTE1
+SELECT num,
+LEAD(num, 1) OVER (ORDER BY id) AS lead,
+LAG(num, 1) OVER (ORDER BY id) AS lag
+FROM logs
 )
-SELECT Distinct ConsecutiveNums FROM CTE2
-where ConsecutiveNums IS NOT NULL
+Select distinct(num) as ConsecutiveNums from cte1
+where num = lead and lag= num and lag=lead
